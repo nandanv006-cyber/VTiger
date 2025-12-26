f9087f0cd42c030a29f5e4852b13fab50cf5732f
Likitha-HE <likithahe456@gmail.com>
2025-12-26 15:22:06 +0530

    Update UpdateInCode.java
    made some changes

--- src/test/java/UpdateInCode.java
+++ src/test/java/UpdateInCode.java
@@ -2,6 +2,10 @@
 public class UpdateInCode {
 	public static void main(String[] args) {
 		System.out.println("----test1----");
+		System.out.println("----test2----");
+		System.out.println("----test3----");
+		System.out.println("----test4----");
+		
 	}
 
 }
