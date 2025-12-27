59302a72d45c6617e6454cf3a189344bea478853
nandanv006-cyber <nandanv006@gmail.com>
2025-12-27 11:57:32 +0530

    Update PracticeGitHub.java

--- src/test/java/assignment/PracticeGitHub.java
+++ src/test/java/assignment/PracticeGitHub.java
@@ -4,16 +4,7 @@ public class PracticeGitHub
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("---Arvind Test Class---");
-		System.out.println("---Arvind Test Class---");
-		System.out.println("---ravi Test Class---");
-		System.out.println("---ravi Test Class---");
-		System.out.println("---ravi Test Class---");
-		System.out.println("---ravi Test Class---");
-		System.out.println("---ravi Test Class---");
 		
-
-		System.out.println("---Arvind Test Class-ed--");
 		
 	} 
 
