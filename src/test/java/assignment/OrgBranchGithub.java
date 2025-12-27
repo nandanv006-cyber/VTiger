c53bb0abfe42184c7aa20c7668c82ed50a58d86a
Arvind <Arvind@LAPTOP-5HVVEL57>
2025-12-27 12:30:53 +0530

    1st commit by arvind

--- src/test/java/assignment/OrgBranchGithub.java
+++ src/test/java/assignment/OrgBranchGithub.java
@@ -4,13 +4,7 @@ public class OrgBranchGithub
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("----OrgBranch----");
-		System.out.println("----OrgBranch----");
-		System.out.println("----OrgBranch----");
-		System.out.println("----OrgBranch----");
-		System.out.println("----OrgBranch----");
-		System.out.println("hii from Ayushi");
-		System.out.println("----OrgBranch-2---");
+		System.out.println("----Arvind Branch---");
 		System.out.println("yo from arvind");
 	}
 
