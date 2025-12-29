c2045d8ed84a5997450adc21e713fad3feb2dfa6
Sneha <Sneha@DESKTOP-JVQJ4UD>
2025-12-29 13:14:48 +0530

    By sneha

--- src/test/java/assignment/DuplicateClass.java
+++ src/test/java/assignment/DuplicateClass.java
@@ -5,6 +5,10 @@ public class DuplicateClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+System.out.println("hii");
+
+
+
 	}
 
 }
