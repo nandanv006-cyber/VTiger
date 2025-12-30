f790631fc8d6c0ff6c76581a0d8a91fd1e74fedb
Nandan V <Nandan V@NandanV>
2025-12-30 17:50:05 +0530

    My latest comit

--- src/test/java/Ayushi/NewSampletTest.java
+++
@@ -1,8 +0,0 @@
-package Ayushi;
-
-public class NewSampletTest {
-	public static void main(String[] args) {
-		System.out.println("Hii from Ayushi");
-	}
-
-}
--- src/test/java/Ayushi/Testtt1.java
+++
@@ -1,8 +0,0 @@
-package Ayushi;
-
-public class Testtt1 {
-	public static void main(String[] args) {
-		
-	}
-
-}
