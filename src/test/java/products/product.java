dc3f8340650deadc82ca2347242bdc1b34df32b1
nandanv006-cyber <nandanv006@gmail.com>
2026-01-03 19:06:11 +0530

    Update product.java

--- src/test/java/products/product.java
+++ src/test/java/products/product.java
@@ -17,6 +17,7 @@ public class product extends BaseClass{
 	{	
 //		TC_07
 		System.out.println("start");
+		System.out.println("start");
 		ExcelUtils eLib = new ExcelUtils();
 		String pathImg="C:\\Users\\Nandan V\\Downloads\\111868_SP781-iPhone-xr.jpg";
 		String Product = eLib.readDataFromExcel("Products", 1, 0);
@@ -65,4 +66,5 @@ public class product extends BaseClass{
 	
 
 }
+
 
