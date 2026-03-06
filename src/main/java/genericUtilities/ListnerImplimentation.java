68f0200531faea81a58e258ba3f7f12e96b6bf79
nandanv006-cyber <nandanv006@gmail.com>
2026-03-06 19:30:47 +0530

    changes by Nandan

--- src/main/java/genericUtilities/ListnerImplimentation.java
+++ src/main/java/genericUtilities/ListnerImplimentation.java
@@ -12,33 +12,33 @@ public class ListnerImplimentation implements ITestListener{
 	ExtentReporter extentReport;
 	ExtentTest test;
 	public static ThreadLocal<ExtentTest> extentTest= new  ThreadLocal<ExtentTest>();
-	@Override
+	
 	public void onTestStart(ITestResult result) {
 		
 		
 	}
 
-	@Override
+	
 	public void onTestSuccess(ITestResult result) {
 		
 	}
 
-	@Override
+	
 	public void onTestFailure(ITestResult result) {
 		
 	}
 
-	@Override
+	
 	public void onTestSkipped(ITestResult result) {
 	
 	}
 
-	@Override
+	
 	public void onStart(ITestContext context) {
 	
 	}
 
-	@Override
+	
 	public void onFinish(ITestContext context) {
 		
 	}
